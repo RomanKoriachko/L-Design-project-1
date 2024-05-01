c7243282273f0ae56017878540145bce323d46a8
Roman Koriachko <roman_koriachko@epam.com>
2024-05-01 12:07:53 +0300

    add photos and photo viewer

---
+++ app.js
@@ -0,0 +1,6 @@
+fsLightboxInstances['first-lightbox'].props.onOpen = function () {
+	console.log('The first lightbox has opened.');
+};
+fsLightboxInstances['second-lightbox'].props.onOpen = function () {
+	console.log('The second lightbox has opened.');
+};
